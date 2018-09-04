fccfb61d64e660fb447583e6d7e3b33c7c95d4da
ALIEN-07DE6V7\Toby <toby.jayne@gmail.com>
2018-09-04 11:17:32 -0500

    Fixing dropdown link
    Fixing dropdown link

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -61,7 +61,7 @@
 						{{Auth::user()->name}} <i class="fa fa-caret-down"> </i>
 				</a>
 					<ul class="dropdown-menu dropdown-user">
-						<li><a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>
+						<li><a href="admin/users/{{Auth::user()->id}}/edit/"><i class="fa fa-user fa-fw"></i> User Profile</a>
 						</li>
 						<li><a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a></li>
 						<li class="divider"></li>
