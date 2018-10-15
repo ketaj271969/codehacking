f1c54fdd92f5b74a800b542ea8f2680f8421adb4
Toby Jayne <TJAYNE@hubgroup.com>
2018-10-15 13:09:56 -0500

    Introducing a bug for comment/replies
    Introducing a bug for comment/replies

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -53,21 +53,73 @@
 @endif
 
     @if(count($comments) > 0 )
+
         @foreach($comments as $comment)
+
             <div class="media">
                 <a class="pull-left" href="#">
-                    <img height="64" class="media-object" src="{{$comment->$photo}}" alt="">
-                </a>
+                   {{-- <img height="64" class="media-object" src="{{$comment->$photo}}" alt="">
+                --}}</a>
                 <div class="media-body">
                     <h4 class="media-heading">{{$comment->author}}
                         <small>{{$comment->created_at->diffForHumans()}}</small>
                     </h4>
                     <p>{{$comment->body}}</p>
                 </div>
-            </div>
+                {{-- Nested Comment --}}
 
+             @if(count($comment->replies) > 0)
+
+                    @foreach ($comment->replies as $reply)
+
+                    <div id="nested-comment" class="media">
+                        <a class="pull-left" href="#">
+                            <img height="64" class="media-object" src="{{$reply->photo}}" alt="">
+                        </a>
+                        <div class="media-body">
+                            <h4 class="media-heading">{{$reply->author}}
+                                <small>{{$reply->created_at->diffForHumans()}}</small>
+                            </h4>
+                            <p>{{$reply->body}}</p>
+                        </div>
+
+                        <div class="comment-reply-container">
+                            <button class="toggle-reply btn btn-primary pull-right">Reply</button>
+                            <div class="comment-reply">
+
+                            {!! Form::open(['method'=>'POST', 'action'=>'CommentRepliesController@createReply'])  !!}
+                                <div class="form-group">
+                                    <input type="hidden" name="comment_id" value="{{$comment->id}}">
+
+                                    {!! Form::label('body','Body:') !!}
+                                    {!! Form::textarea('body', null, ['class'=>'form-control','rows'=>1]) !!}
+
+                                </div>
+                            </div>
+                        <div="form-group">
+                        {!! Form::submit('submit', ['class'=>'btn btn-primary']) !!}
+                        </div>
+
+                    {!! Form::close() !!}
+                    </div>
+                    @endforeach
+             @endif
+            </div>
+            </div>
             @endforeach
 
     @endif
 
     @stop
+
+@section('scripts')
+
+    <script>
+        $(".comment-reply-container .toggle-reply").click(function(){
+
+            $(this).next().slideToggle("slow");
+        });
+
+    </script>
+
+@stop
